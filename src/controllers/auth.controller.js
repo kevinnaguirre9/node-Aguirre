325e65b1b6269322c5b3328b4d771913aab9d626
Kevin Aguirre <kevinnaguirre9@gmail.com>
2021-09-03 15:26:31 -0500

    SignIn method created

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -31,4 +31,27 @@ export const signUp = async (req, res) => {
 }
 
 export const signIn = async (req, res) => {
+     try {
+          const {email, password} = req.body;
+          const userFound = await User.findOne({ email: email }).populate(
+            "roles"
+          );
+      
+          if (!userFound) return res.status(400).json({ message: "User Not Found" });
+          
+          const matchPassword = await User.comparePassword(
+            password,
+            userFound.password
+          );
+      
+          if (!matchPassword) return res.status(401).json({token: null, message: "Invalid Password"});
+      
+          const token = jwt.sign({ id: userFound._id }, config.SECRET, {
+            expiresIn: 86400, // 24 hours
+          });
+      
+          res.json({ token });
+     } catch (error) {
+          console.log(error);
+     }
 }
