c624cdde9f5a2b8f53a17ff832ad788352f2cd60
Kevin Aguirre <kevinnaguirre9@gmail.com>
2021-09-03 10:58:28 -0500

    Products get request created

--- src/index.js
+++ src/index.js
@@ -1,12 +1,5 @@
 import app from './app'
-import morgan from 'morgan'
 
 app.listen(3000)
-
-app.use(morgan('combined'))
-
-app.get('/', (req, res) => {
-     res.json('Welcome')
-})
 
 console.log('Server listening on port', 3000)
