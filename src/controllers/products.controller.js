1a0ff123ada94af7caf0ea7157c4633103aa75aa
Kevin Aguirre <kevinnaguirre9@gmail.com>
2021-09-03 11:03:57 -0500

    Basic products routes created

---
+++ src/controllers/products.controller.js
@@ -0,0 +1,19 @@
+export const createProduct = async (req, res) => {
+
+}
+
+export const getProducts = async (req, res) => {
+     res.json('get Products')
+}
+
+export const getProductById = async (req, res) => {
+     
+}
+
+export const updateProductById = async (req, res) => {
+     
+}
+
+export const deleteProduct = async (req, res) => {
+     
+}
--- src/routes/products.routes.js
+++ src/routes/products.routes.js
@@ -1,9 +1,12 @@
 import {Router} from 'express'
+import * as productCtrl from '../controllers/products.controller'
 
 const router = Router()
 
-router.get('/', (req, res) =>{
-     res.json('getting products')
-})
+router.post('/', productCtrl.createProduct)
+router.get('/', productCtrl.getProducts)
+router.get('/', productCtrl.getProductById)
+router.put('/', productCtrl.updateProductById)
+router.delete('/', productCtrl.deleteProduct)
 
 export default router;
